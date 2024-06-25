60c535d4ffe1388fdedf39987767008e5b9cf2dd
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-06-25 18:53:56 -0400

    updates
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+resource "random_id" "suffix" {
+
+  byte_length = 8
+}
+
 resource "google_project" "project" {
-  name       = "${var.project_name}"
-  project_id = "${var.project_id}"
+  name       = "${var.project_name}-${random_id.suffix.hex}"
+  project_id = "${var.project_id}-${random_id.suffix.hex}"
   org_id     = "${var.gcp_org_id}"
   billing_account = "${var.billing_account}"
 }
